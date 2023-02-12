d5775d97906c7745d3b83a51dee8cc2be4fb3b1a
Khurshid <124437219+Ravshanovich08@users.noreply.github.com>
2023-02-12 15:41:29 +0500

    lesson for jq

---
+++ uy ishi/index.js
@@ -0,0 +1,18 @@
+function swap (x, y) {
+
+    let a = 5;
+    let b = 11;
+
+
+    console.log("a = ", a);
+    console.log("b = ", b);
+
+    console.log("Swaping...");
+    a = a;
+    b = b;
+
+    console.log("a = ", b);
+    console.log("b = ", a);
+}
+
+swap (5, 11);
